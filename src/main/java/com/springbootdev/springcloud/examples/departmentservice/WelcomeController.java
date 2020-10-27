d4f1bf26f271b478d27a8c1817e9514f4cfe1927
Arun Balaji R <iamarunbalaji@icloud.com>
2020-10-27 13:11:37 +0530

    changes

--- src/main/java/com/springbootdev/springcloud/examples/departmentservice/WelcomeController.java
+++ src/main/java/com/springbootdev/springcloud/examples/departmentservice/WelcomeController.java
@@ -1,12 +1,18 @@
 package com.springbootdev.springcloud.examples.departmentservice;
 
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@RefreshScope
 @RestController
 public class WelcomeController {
-	@GetMapping("hello")
-	public String hello() {
-		return "world";
+	@Value("${app.service-name}")
+	private String serviceName;
+
+	@GetMapping("/service")
+	public String getServiceName() {
+		return "service name [" + this.serviceName + "]";
 	}
 }
